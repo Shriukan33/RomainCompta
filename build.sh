7524d7e7214d94431edd675e713fa74cafdf46f6
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-11-03 13:39:44 +0100

    Remove temp user

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('temp_user', '', 'some_random_password')" | python manage.py shell
