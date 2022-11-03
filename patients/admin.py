db22733779b9f91fc278e2d9ba946b18c3dca7f2
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2022-11-03 12:23:35 +0100

    Add patient model

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,85 @@
 from django.contrib import admin
+from django.urls import reverse
+from .models import Patient, SessionPatient
+from ventes.models import FactureVente
+from django.utils.html import format_html
 
-# Register your models here.
+
+class FactureVenteInline(admin.TabularInline):
+    model = FactureVente
+    fields = [
+        "_numero_de_facture",
+        "type_de_facture",
+        "date",
+        "montant",
+        "mode_de_reglement",
+        "date_du_reglement",
+        "statut_de_paiement",
+        "_document_lie",
+    ]
+    readonly_fields = ["_document_lie", "_numero_de_facture"]
+    show_change_link = True
+    extra = 0
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+    def _document_lie(self, obj):
+        human_readable_name = obj.document_lie.name.split("/")[-1]
+        return format_html(
+            f'<a href="{obj.document_lie.url}">{human_readable_name}</a>'
+        )
+
+    def _numero_de_facture(self, obj):
+        """Return a link to change the object"""
+        object_link = reverse(
+            "admin:{0}_{1}_change".format(obj._meta.app_label, obj._meta.model_name),
+            args=(obj.pk,),
+        )
+        return format_html(f'<a href="{object_link}">{obj.numero_de_facture}</a>')
+
+
+class SessionPatientInline(admin.TabularInline):
+    model = SessionPatient
+    extra = 0
+    show_change_link = True
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class PatientAdmin(admin.ModelAdmin):
+
+    # Display related bills with tabular inlines
+    inlines = [FactureVenteInline, SessionPatientInline]
+
+    list_display = (
+        "nom",
+        "prenom",
+        "sexe",
+        "categorie",
+        "date_naissance",
+        "adresse",
+        "telephone",
+        "email",
+        "notes",
+    )
+    list_filter = ("sexe", "categorie")
+    search_fields = ("nom", "prenom")
+    ordering = ("nom",)
+
+
+class SessionPatientAdmin(admin.ModelAdmin):
+    list_display = ("date", "patient", "notes", "prestation", "facture_liee")
+    search_fields = (
+        "patient__nom",
+        "patient__prenom",
+        "facture_liee__numero_de_facture",
+    )
+    date_hierarchy = "date"
+    ordering = ("-date",)
+
+
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(SessionPatient, SessionPatientAdmin)
--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    sexe = models.CharField(max_length=1, choices=[('M', 'Masculin'), ('F', 'Féminin')])
+    categorie = models.CharField(max_length=1, choices=[('A', 'Adulte'), ('E', 'Enfant')])
+    date_naissance = models.DateField(verbose_name="Date de naissance")
+    adresse = models.CharField(max_length=100, blank=True, null=True)
+    telephone = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nom} {self.prenom}"
+
+    class Meta:
+        verbose_name = "Patient"
+        verbose_name_plural = "Patients"
+        ordering = ['nom']
+
+
+class SessionPatient(models.Model):
+    date = models.DateField()
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    notes = models.TextField(blank=True, null=True)
+    prestation = models.ForeignKey("ventes.Prestation", on_delete=models.CASCADE)
+    facture_liee = models.ForeignKey("ventes.FactureVente", on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.patient} - {self.date}"
+
+    class Meta:
+        verbose_name = "Session patient"
+        verbose_name_plural = "Sessions patient"
+        ordering = ['date']
